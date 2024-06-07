398b2c33e8c041ae3d8ff1ce2bf4c5ad095fc37e
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-06-07 11:26:46 +0530

    functions done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import Input from "./components/Input";
+import Board from "./components/Board";
 import { useState } from "react";
-import './index.css';
-
+import "./index.css";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
 
   console.log(taskList);
 
-
   return (
     <>
-    <div className="flex flex-col items-center justify-center py-8 gap-4">
-      <h1 className="text-xl font-semibold">To Do Board</h1>
-      <Input taskList={taskList} setTaskList={setTaskList}/>
-      <div>
-        {taskList.map((task, index) => (
-         <li key={index}>{task}</li>
-        ))}
-      </div>
+      <div className="flex flex-col items-center justify-center py-8 gap-4">
+        <h1 className="text-xl font-semibold">To Do Board</h1>
+        <Input taskList={taskList} setTaskList={setTaskList} />
+        <div className="grid grid-cols-3">
+          {taskList.map((task, index) => (
+            <Board
+              key={index}
+              index={index}
+              task={task}
+              taskList={taskList}
+              setTaskList={setTaskList}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
 }
 
 export default App;
-
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,27 +1,30 @@
 import { useState } from "react";
 
-const Input = ({taskList, setTaskList}) => {
+const Input = ({ taskList, setTaskList }) => {
   const [input, setInput] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setTaskList([...taskList, input]);
     setInput("");
-  }
+  };
 
   return (
     <>
       <form className="flex flex-row items-center gap-3">
         <input
-        className="border rounded-lg py-1 px-2.5 text-lg"
+          className="border rounded-lg py-1 px-2.5 text-lg"
           type="text"
           placeholder="Add a new task"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <button 
-        className="bg-blue-500 text-white rounded-lg py-1 px-2.5"
-        onClick={handleSubmit}>Add</button>
+        <button
+          className="bg-blue-500 text-white rounded-lg py-1 px-2.5 hover:opacity-80"
+          onClick={handleSubmit}
+        >
+          Add
+        </button>
       </form>
     </>
   );
