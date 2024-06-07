a914dc2f7d8aa291db796af96e5453bd9f1cd85f
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-06-07 10:47:35 +0530

    task list added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Input from "./components/Input";
+import { useState } from "react";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  console.log(taskList);
+
+
   return (
     <>
       <h1>To Do Board</h1>
+      <Input taskList={taskList} setTaskList={setTaskList}/>
+      <div>
+        {taskList.map((task) => (
+         <li>{task}</li>
+        ))}
+      </div>
     </>
   );
 }
